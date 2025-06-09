4f8f5316a0f25e0faa9d66d74b1ad4351ad76af4
gradgear <gradgear7@gmail.com>
2025-06-09 17:53:34 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/AppNavbar.tsx
+++ src/components/layout/AppNavbar.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import * as React from 'react'; // Added this line
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Button } from '@/components/ui/button';
